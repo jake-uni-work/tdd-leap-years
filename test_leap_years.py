ccf0031aae7c48c10745b68beefb36b4563c209f
Jake <jake@jakemt04.xyz>
2026-05-01 09:41:09 +0100

    feat: multiple of 4 is a leap year

--- test_leap_years.py
+++ test_leap_years.py
@@ -1,9 +1,16 @@
 import unittest
 
+from leap_years import is_leap_year
+
 class SmokeTest(unittest.TestCase):
     def test_smoke(self):
         self.assertEqual(1+1, 2)
         self.assertNotEqual(1+1, 3)
+        
+class LeapYearTests(unittest.TestCase):
+    def test_multiples_of_4_are_leap_years(self):
+        self.assertEqual(is_leap_year(2024), True)
+        self.assertEqual(is_leap_year(2026), False)
         
 if __name__ == "__main__":
     unittest.main()
