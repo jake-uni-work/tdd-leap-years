38d7158779cf6d81f3dcb48a258b60a4c8bc2c29
Jake <jake@jakemt04.xyz>
2026-05-01 09:43:51 +0100

    feat: mutiple of 100 is not a leap year

--- leap_years.py
+++ leap_years.py
@@ -1,3 +1,5 @@
 def is_leap_year(year: int) -> bool:
     """Returns True if the supplied year is a leap year, False otherwise"""
-    return year % 4 == 0
+    if year % 4 == 0:
+        return year % 100 != 0
+    return False
--- test_leap_years.py
+++ test_leap_years.py
@@ -11,6 +11,9 @@ class LeapYearTests(unittest.TestCase):
     def test_multiples_of_4_are_leap_years(self):
         self.assertEqual(is_leap_year(2024), True)
         self.assertEqual(is_leap_year(2026), False)
+    
+    def test_multiple_of_100_is_not_leap_year(self):
+        self.assertEqual(is_leap_year(1800), False)
         
 if __name__ == "__main__":
     unittest.main()
